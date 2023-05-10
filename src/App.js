4cabf5876ea9db3d913ab5c6f393f49ae18b3661
Sieun <si990819@gmail.com>
2023-05-10 12:07:09 +0900

    기본옵션 셋팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+//APP : 사용자가 만든 새로운 컴포넌트
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+   <A>
+
+    <h1>안녕</h1>
+    <h2>안녕2</h2>
+  </A>
   );
 }
 
 export default App;
+
+
+const A=styled.h1`
+  background-color : yellow;
+  color: black;
+  display
+  ali
+`
